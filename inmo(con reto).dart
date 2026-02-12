eb068ad556f4e46caef2463acd51510d575a7c18
Yerald123-cpu <020200961a@uandina.edu.pe>
2026-02-11 23:12:23 -0500

    Create inmo(con reto).dart

---
+++ inmo(con reto).dart
@@ -0,0 +1,72 @@
+sealed class Inmueble {}
+
+class Casa extends Inmueble {
+  final int habitaciones;
+  final double metrosCuadrados;
+  Casa(this.habitaciones, this.metrosCuadrados);
+}
+
+class Apartamento extends Inmueble {
+  final int piso;
+  final bool tieneAscensor;
+  Apartamento(this.piso, this.tieneAscensor);
+}
+
+typedef InfoFinanciera = ({
+  double lat,
+  double lng,
+  String zona,
+  double precio
+});
+
+void procesarCatalogo(List<(Inmueble, InfoFinanciera)> catalogo) {
+  print('--- REPORTE DE PROPIEDADES INMODART ---\n');
+
+  for (var (inmueble, info) in catalogo) {
+
+    var (:lat, :lng, :zona, :precio) = info;
+
+    String categoria = switch (inmueble) {
+      Casa(habitaciones: > 4) => 'Mansion Familiar',
+      Casa() => 'Casa Estandar',
+      Apartamento(piso: > 10, tieneAscensor: true) => 'Penthouse / Atico',
+      Apartamento(tieneAscensor: false) => 'Dep. Acceso por Escala',
+      _ => 'Propiedad General',
+    };
+
+    double precioFinal = switch (precio) {
+      < 50000 => precio * 0.95,
+      >= 50000 && <= 150000 => precio,
+      _ => precio * 1.10,
+    };
+
+    //  RETO: descuento adicional de $2000
+    if (inmueble case Apartamento(piso: > 5, tieneAscensor: false)) {
+      precioFinal -= 2000;
+    }
+
+    print('Tipo: $categoria');
+    print('Ubicacion: [$lat, $lng] en Zona: $zona');
+    print('Precio Final: \$${precioFinal.toStringAsFixed(2)}\n');
+  }
+}
+
+void main() {
+
+  var listaPropiedades = <(Inmueble, InfoFinanciera)>[
+    (
+      Casa(5, 250.0),
+      (lat: -12.04, lng: -77.03, zona: 'Miraflores', precio: 180000.0)
+    ),
+    (
+      Apartamento(12, true),
+      (lat: -12.08, lng: -76.99, zona: 'San Isidro', precio: 120000.0)
+    ),
+    (
+      Apartamento(6, false), //  este activará el reto
+      (lat: -12.12, lng: -77.01, zona: 'Surquillo', precio: 45000.0)
+    ),
+  ];
+
+  procesarCatalogo(listaPropiedades);
+}
